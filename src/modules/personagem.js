bdc1a84d3e9125753256df2ad22503ffd1e69922
Vagner Santos <dbavagner@gmail.com>
2025-07-08 11:52:54 -0300

    Primeira alteração do projeto

--- src/modules/personagem.js
+++ src/modules/personagem.js
@@ -1,11 +1,18 @@
 export class Personagem {
     nome
-    vida
-    mana
+    vida = 100
+    mana = 100
     level
     tipo
     descricao
 
+    constructor(nome, level, tipo){
+        this.nome = nome
+        this.level= level
+        this.tipo = tipo
+
+    }
+    
     obterInsignia() {
         if (this.level >= 5){
             return `Implacavel ${this.tipo}`
